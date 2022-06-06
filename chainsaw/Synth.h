12108a38eb13d17ae4ff716358802d3c9f9703a7
Cesium1289 <Carson.hansen@gmail.com>
2022-06-06 04:54:17 -0700

    Updated graphics

--- chainsaw/Synth.h
+++ chainsaw/Synth.h
@@ -13,10 +13,10 @@ double volume = 8.0;
 
 struct Midi
 {
-	float ramp = 0.5f; // 0.0 - 0.5
-	int octave = 4; //  1 - 8 
+	float ramp = 0.0f; // 0.0 - 0.5
+	int octave = 1; //  1 - 8 
 	float amplitude = 261.0f; //27.5 - 12543.85
-	float volume = 8.0f; // 0.0 - 10.0
+	float volume = 5.0f; // 0.0 - 10.0
 	int midiNoteNumber = 60; // 21 - 115
 	float bpm = 60.0f; // 0 - sizeof(float)
 	std::vector<int> midiRectValues; // holds the midi key value for each rectangle
